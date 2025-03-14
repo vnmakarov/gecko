99a1b025db5de3b44dc1fede6cd87ba734b5b7f5
Vladimir N. Makarov <vmakarov@redhat.com>
2025-03-14 17:55:59 -0400

    Add test26 for ambigous translation.

---
+++ test/test26.c
@@ -0,0 +1,20 @@
+/* This file is a part of Gecko Parser (GLR Parser) project.
+   Copyright (C) 2025 Vladimir Makarov <vmakarov.gcc@gmail.com>.
+*/
+
+#include <stdlib.h>
+
+#include "common.h"
+
+static const char *input = "a+a+a";
+
+static const char *description
+  = "\n"
+    "E : 'a'         # 0\n"
+    "  | E '+' E     # plus (0 2)\n"
+    "  ;\n";
+
+int main (int argc, char **argv) {
+  test_complex_parse (false, true, false, 3, true, argc, argv);
+  exit (0);
+}
